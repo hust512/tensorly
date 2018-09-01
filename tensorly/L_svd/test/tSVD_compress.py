f5c00b7ade2ee515cfd127aa1b42f20f7b66e4ec
Xiao-Yang Liu <xl2427@columbia.edu>
2018-09-01 16:17:02 +0800

    Update tSVD_compress.py

--- tensorly/L_svd/test/tSVD_compress.py
+++ tensorly/L_svd/test/tSVD_compress.py
@@ -1,51 +1,72 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug 30 11:05:08 2018
+Created on Fri Aug 31 09:29:49 2018
 
-@author: lsj
+@author: dayangyang
 """
+import matplotlib.pyplot as plt
 import numpy as np
-from L_trans import trans, invtrans
+import sys
+sys.path.append("..")
 from L_svd import L_svd
 from L_prod import L_prod
-#from proxF import proxF
-#from FindX import FindX
-def L_svd_compress(M, k2, flag = 'fft'):
-   [n1, n2, n3] = M.shape
-   n0 = min(n1, n2)
-#   if k2>n0 or k2<1:
-#       raise ValueError('k2 is illegal')
-   U, S, V = L_svd(M, flag = 'fft')
-#   U = trans(U, flag)
-#   S = np.zeros((S.shape), dtype = complex)
-   S = trans(S, flag)
-#   V = trans(V, flag)
-   sTrueV = np.zeros((n0, n3), dtype = complex)
-   for i in range(n3):
-       s = S[:, :, i]
-       s = np.diag(s)
-       sTrueV[:, i] = s
-   k2 = FindX(sTrueV, k2)
-   sTrueV = proxF(sTrueV, k2)
-   for i in range(n0):
-       for j in range(n3):
-           S[i, i, j] = sTrueV[i, j]
-   S = invtrans(S, flag).real
-   Mm = L_prod(L_prod(U, S, flag), V, flag).real
-#   print(Mm.shape)
-   return Mm
+from L_svd_compress import L_svd_compress
+from L_svd_compress_tub import L_svd_compress_tub
+import scipy.io as sio
+#def t_svd_test():
+M = sio.loadmat('video.mat')
+M = M['T']
+n1, n2, n3 = M.shape
+tol = n1*n2*n3
+n0 = min(n1, n2)*n3
+###################################################
+#                 tSVD Compress
+###################################################
 
 
-def FindX(M, k2):
-    M = M.reshape(1, M.size)
-#    M = numpy.fromnumeric.sort(M)
-    M = np.sort(M)
-    return M[0, k2]
+RSE = []
+I = []
+f = 'fft'
+for i in range(10):
+    I.append(i+1)
+    k2 = np.ceil(tol/(i+2))
+    k2 = n0-int(k2/n2)
+    M2 = L_svd_compress(M, k2-1, flag = f)
+    RSE.append(np.linalg.norm(M - M2)/np.linalg.norm(M))
+RSE= np.log10(RSE)*20
 
-def proxF(x, k2):
-#    s = 1- min(k2/abs(x), 1)
-    x[(k2/abs(x))>1] = 0
-#    s = 1-x
-#    x = x * s
-#    x = x + k2
-    return x
+
+####################################################
+#  tSVD_tubal Compress
+####################################################
+#M1 = L_svd_compress_tub(M, 5, flag = 'fft')
+RSE_tub = []
+for i in range(10):
+    k2 = np.ceil(n1*n2/((n1+n2+1)*(i+1)))
+    k2 = int(k2)
+#    print(k2)
+    M1 = L_svd_compress_tub(M, k2, flag = 'fft')
+    RSE_tub.append(np.linalg.norm(M - M1)/np.linalg.norm(M))
+RSE_tub = np.log10(RSE_tub)*20
+#plt.plot(RSE_tub, '*-')
+
+
+####################################################
+#    plot figure
+####################################################
+plt.figure(1)
+plt.plot(I, RSE,'*-')
+plt.title('basketball')
+plt.xlabel('compress ratio')
+plt.ylabel('RSE in dB')
+plt.plot(I, RSE_tub, '^-')
+plt.legend(('tSVD compress','tSVD_tubal Compress'))
+#M3 = L_svd_compress(M, 3000, flag = f)
+#ratio3 = n1*n2*n3/((n0-3000)*(n1 + n2 + 1))
+#plt.figure(2)
+#plt.subplot(121)
+#plt.imshow(M3[:, :, 1],cmap = 'gray')
+#plt.title('t_svd compress')
+#plt.subplot(122)
+#plt.imshow(M[:, :, 1], cmap = 'gray')
+#plt.title('origin')
